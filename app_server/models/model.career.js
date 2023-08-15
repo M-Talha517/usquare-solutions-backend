d98f394a40d90ed3017de15bd6ce4686950f7971
Muhammad Usama <mosama4u@gmail.com>
2023-08-15 17:13:28 +0500

    career done

--- app_server/models/model.career.js
+++ app_server/models/model.career.js
@@ -1,29 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const schema = mongoose.Schema;
 
 // Career Schema
 const careerSchema = new schema({
-    title:{
-        type: String
-    },
-    position:{
-        type: Number
-    },
-    category_id:{
-        type: mongoose.Schema.Types.ObjectId, ref: 'Category',
-    },
-    body:{
-        type: String
-    },
-    state:{
-        type: String, default: "active"
-    },
-    date_time:{
-        type: Date, default: Date.now 
-    },
-  
-})
+  title: {
+    type: String,
+  },
+  position: {
+    type: Number,
+  },
+  category_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
+  body: {
+    type: String,
+  },
+  state: {
+    type: String,
+    default: "active",
+  },
+  date_time: {
+    type: Date,
+    default: Date.now,
+  },
+  experience: {
+    type: String,
+  },
+  jobType: {
+    type: String,
+  },
+});
 
-
-const Career= module.exports = mongoose.model('Career',careerSchema);
+const Career = (module.exports = mongoose.model("Career", careerSchema));
